eb2f4d0728c16b2eedf871d1c2455474c0bd9035
JOJOeth <102779647+JOJOeth@users.noreply.github.com>
2022-07-27 22:08:05 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,7 @@
+## Distributed Training
+#CUDA_VISIBLE_DEVICES=0 python main.py --nodes 2 --nr 0
+#CUDA_VISIBLE_DEVICES=1 python main.py --nodes 2 --nr 1
+#CUDA_VISIBLE_DEVICES=2 python main.py --nodes 2 --nr 2
+#CUDA_VISIBLE_DEVICES=N python main.py --nodes 2 --nr 3
+
+python main.py
